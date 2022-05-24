c7fac83f6518f9987f5e43bb2ab7d53394d7095e
JANGTAEHEE <jangth0655@gmail.com>
2022-05-24 23:23:39 +0900

    setup project

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import "dotenv/config";
+import express from "express";
+import http from "http";
+import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import { resolvers, typeDefs } from "./schema";
+
+const PORT = process.env.PORT;
+
+async function startApolloServer() {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const apollo = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await apollo.start();
+  apollo.applyMiddleware({ app, path: "/" });
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: PORT }, resolve)
+  );
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}${apollo.graphqlPath}`
+  );
+}
+
+startApolloServer();
